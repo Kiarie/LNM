3785146dfb70321bbb72e0577db5c7853666e303
jSon <kiariemburus@gmail.com>
2015-11-30 12:35:09 +0300

    More PARAMS

--- myql_test.cs
+++ myql_test.cs
@@ -20,7 +20,7 @@ namespace Mysql_test
 			 conn.Open();
 			 try
 			 {
-			 string strsql = "SELECT "+arr[0]+".`aff_name`, "+arr[1]+".`trans_ammount`, `affs_trans`.`commission`, `months`.`month`, `merchants`.`merchant` FROM `affs_trans` INNER JOIN `affiliates` ON `affs_trans`.`aff_id` = `affiliates`.`id` INNER JOIN `months` ON `affs_trans`.`mth_id` = `months`.`mid`	INNER JOIN `merchants` ON `affs_trans`.`mer_id` = `merchants`.`id` ";
+			 string strsql = "SELECT "+arr[0]+".`aff_name`, "+arr[1]+".`trans_ammount`, "+arr[1]+".`commission`, `months`.`month`, `merchants`.`merchant` FROM `affs_trans` INNER JOIN `affiliates` ON `affs_trans`.`aff_id` = `affiliates`.`id` INNER JOIN `months` ON `affs_trans`.`mth_id` = `months`.`mid`	INNER JOIN `merchants` ON `affs_trans`.`mer_id` = `merchants`.`id` ";
 			OdbcCommand cmd = new OdbcCommand(strsql, conn);
 			OdbcDataReader dr = cmd.ExecuteReader();
 			
